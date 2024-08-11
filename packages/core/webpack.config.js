3bb65d74d58ce93e806d169165b9f836cc87535d
rindeal <dev.rindeal@gmail.com>
2024-08-11 06:30:16 +0200

    Create webpack.config.js

---
+++ packages/core/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/core.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  output: {
+    filename: 'core.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'commonjs2'
+  },
+  devtool: 'source-map'
+};
